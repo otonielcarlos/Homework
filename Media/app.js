837d9aa9cc96a2816ef6a6a9b1efe50880073216
Otoniel Carlos <otonielcarlosf@gmail.com>
2015-11-13 09:29:14 -0700

    Commit to Media

--- Media/app.js
+++ Media/app.js
@@ -1,1 +1,37 @@
-var app = angular.module("MyMedia", ["ngRoute", "ngAnimate"]);
+var app = angular.module("MyMedia", []);
+
+app.controller("App", ["$scope", "$http", function ($scope, $http) {
+
+    $scope.movies = [];
+    var url = "http://www.omdbapi.com/";
+    var s = "";
+
+    $scope.search = function () {
+        $http.get(url, {
+            params: {
+                "s": $scope.titleSearch
+
+            },
+        }).then(function (response) {
+            $scope.movies = response.data.Search;
+
+            angular.forEach($scope.movies, function (movie) {
+
+                $http.get(url, {
+                    params: {
+                        "t": movie.Title
+
+                    },
+                }).then(function (response) {
+
+                    movie.Plot = response.data.Plot;
+                    movie.Genre = response.data.Genre;
+
+                })
+            })
+        }, function (response) {
+            console.log("There was an error");
+
+        });
+    }
+}]);
